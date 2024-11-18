29109e85c57fb6cfcf6739ea814a2d3859522c93
antoniorusso2 <russoantonio303@outlook.it>
2024-11-18 19:23:13 +0100

    ADD: static assets available (imgs)

--- app.js
+++ app.js
@@ -1,53 +1,55 @@
-console.log("app");
+// console.log("app");
 
-const express = require("express");
+const express = require('express');
 
 const app = express();
 const port = 3000;
 
 const posts = [
   {
-    title: "Ciambellone",
-    content: "Ricetta ciambellone",
-    imgUrl: "/public/imgs/ciambellone.jpeg",
-    tags: ["stringa", "stringa", "stringa"]
+    title: 'Ciambellone',
+    content: 'Ricetta ciambellone',
+    imgUrl: '/imgs/ciambellone.jpeg',
+    tags: ['stringa', 'stringa', 'stringa']
   },
   {
-    title: "Cracker alla barbabietola",
-    content: "Ricetta cracker alla barbabietola",
-    imgUrl: "/public/imgs/cracker_barbabietola.jpeg",
-    tags: ["stringa", "stringa", "stringa"]
+    title: 'Cracker alla barbabietola',
+    content: 'Ricetta cracker alla barbabietola',
+    imgUrl: '/imgs/cracker_barbabietola.jpeg',
+    tags: ['stringa', 'stringa', 'stringa']
   },
   {
-    title: "Pane fritto dolce",
-    content: "Ricetta pane fritto dolce",
-    imgUrl: "/public/imgs/pane_fritto_dolce.jpeg",
-    tags: ["stringa", "stringa", "stringa"]
+    title: 'Pane fritto dolce',
+    content: 'Ricetta pane fritto dolce',
+    imgUrl: '/imgs/pane_fritto_dolce.jpeg',
+    tags: ['stringa', 'stringa', 'stringa']
   },
   {
-    title: "Pasta Barbabietola",
-    content: "Ricetta ciambellone",
-    imgUrl: "/public/imgs/past_barbabietola.jpeg",
-    tags: ["stringa", "stringa", "stringa"]
+    title: 'Pasta Barbabietola',
+    content: 'Ricetta ciambellone',
+    imgUrl: '/imgs/past_barbabietola.jpeg',
+    tags: ['stringa', 'stringa', 'stringa']
   },
   {
-    title: "Torta paesana",
-    content: "Ricetta torta paesana",
-    imgUrl: "/public/imgs/torta_paesana_jpeg",
-    tags: ["stringa", "stringa", "stringa"]
+    title: 'Torta paesana',
+    content: 'Ricetta torta paesana',
+    imgUrl: '/imgs/torta_paesana.jpeg',
+    tags: ['stringa', 'stringa', 'stringa']
   }
 ];
 
-app.get("/", (req, res) => {
+app.use(express.static('public')); //resa pubblica degli asset statici
+
+app.get('/', (req, res) => {
   console.log(req);
-  res.send("My server");
+  res.send('Server del mio blog');
 }); // url for test 'http://localhost:3000'
 app.listen(3000, () => {
   console.log(`Server listening on port ${port}`);
 });
 
 //array rotta lista almeno 5 post
-app.get("/bacheca", (req, res) => {
+app.get('/bacheca', (req, res) => {
   const postCounter = posts.length;
 
   const myResponse = {
