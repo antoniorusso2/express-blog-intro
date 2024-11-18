ff3ad516af8df5627f1e42f3980e259d479adfe8
antoniorusso2 <russoantonio303@outlook.it>
2024-11-18 17:20:52 +0100

    ADD: server listening and test for response

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log("app");
+
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("My server");
+}); // url for test 'http://localhost:3000'
+app.listen(3000, () => {
+  console.log(`Server listening on port ${port}`);
+});
