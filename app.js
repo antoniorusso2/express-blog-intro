c38a52f25bdd3bbf08468becda610b10a83e2168
antoniorusso2 <russoantonio303@outlook.it>
2024-11-18 18:54:31 +0100

    ADD: imgs folder and posts json response

--- app.js
+++ app.js
@@ -5,9 +5,49 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const posts = [
+  {
+    title: "Ciambellone",
+    content: "Ricetta ciambellone",
+    imgUrl: "/public/imgs/ciambellone.jpeg",
+    tags: ["stringa", "stringa", "stringa"]
+  },
+  {
+    title: "Cracker alla barbabietola",
+    content: "Ricetta cracker alla barbabietola",
+    imgUrl: "/public/imgs/cracker_barbabietola.jpeg",
+    tags: ["stringa", "stringa", "stringa"]
+  },
+  {
+    title: "Pane fritto dolce",
+    content: "Ricetta pane fritto dolce",
+    imgUrl: "/public/imgs/pane_fritto_dolce.jpeg",
+    tags: ["stringa", "stringa", "stringa"]
+  },
+  {
+    title: "Pasta Barbabietola",
+    content: "Ricetta ciambellone",
+    imgUrl: "/public/imgs/past_barbabietola.jpeg",
+    tags: ["stringa", "stringa", "stringa"]
+  },
+  {
+    title: "Torta paesana",
+    content: "Ricetta torta paesana",
+    imgUrl: "/public/imgs/torta_paesana_jpeg",
+    tags: ["stringa", "stringa", "stringa"]
+  }
+];
+
 app.get("/", (req, res) => {
+  console.log(req);
   res.send("My server");
 }); // url for test 'http://localhost:3000'
 app.listen(3000, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+//array rotta lista almeno 5 post
+app.get("/bacheca", (req, res) => {
+  res.json(posts);
+});
+//array rotta bacheca ==> oggetto json con lista dei post e il conteggio partendo da un array
